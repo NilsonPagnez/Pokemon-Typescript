f1e88f741fe2b63ba5db2e45678ffc42252dc891
Nilson Pagnez <nilson.pagneto@gmail.com>
2023-08-15 16:00:47 -0300

    select pronto, apenas necessitando modificar visual

--- src/components/initalPage/carousel/SearchFilter/SearchFilter.tsx
+++ src/components/initalPage/carousel/SearchFilter/SearchFilter.tsx
@@ -1,46 +1,55 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { api } from '../../../../custom/apiCalls/api';
 import style from './searchFilter.module.scss';
 import Select from 'react-select';
+import debounce from 'lodash/debounce';
 
 interface Pokemon {
   name: string;
 }
 
 function SearchFilter() {
-  const [allPokemon, setAllPokemon] = useState<Pokemon[]>([]);
+  const [options, setOptions] = useState<Pokemon[]>([]);
   const [loading, setLoading] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const debouncedSearch = debounce(fetchPokemons, 300);
 
-  useEffect(() => {
-    async function getPokemons() {
-      try {
-        setLoading(true);
-        const response = await api.get(`/pokemon?limit=1000&offset=0`);
-        const mapResponseToValuesAndLabels = (data: any) => ({
-          value: data.name,
-          label: data.name,
-        });
-        setAllPokemon(response.data.results.map(mapResponseToValuesAndLabels));
-      } catch (error) {
-        console.log('Error: ', error);
-      } finally {
-        setLoading(false);
-      }
+  async function fetchPokemons(query: string) {
+    try {
+      setLoading(true);
+      const response = await api.get(`/pokemon?limit=10000&offset=0&name=${query}`);
+      const mapResponseToValuesAndLabels = (data: any) => ({
+        value: data.name,
+        label: data.name,
+      });
+      setOptions(response.data.results.map(mapResponseToValuesAndLabels));
+    } catch (error) {
+      console.log('Error: ', error);
+    } finally {
+      setLoading(false);
     }
+  }
 
-    getPokemons();
-  }, []);
-
+  const handleInputChange = (newValue: string) => {
+    setInputValue(newValue);
+    if (newValue) {
+      debouncedSearch(newValue);
+    } else {
+      setOptions([]);
+    }
+  };
 
   return (
     <div className={style.SearchDiv}>
-      <label htmlFor="pokemon">Select your Pokemon</label>
+      <label htmlFor="pokemon" onClick={()=> console.log(options)}>Select your Pokemon</label>
       <Select
-        options={allPokemon}
+        options={options}
         isLoading={loading}
-        placeholder="Selecione uma opção"
+        inputValue={inputValue}
+        onInputChange={handleInputChange}
+        placeholder="Search your Pokemon"
+        menuIsOpen={inputValue.length > 0}
       />
-
     </div>
   );
 }
